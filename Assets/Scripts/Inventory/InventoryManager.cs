4ecb6a252a29a055af8500ad288466085989d4bf
akvroeg <vroeginday.a@northeastern.edu>
2024-06-03 15:15:32 -0400

    Inventory added to all scenes

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public List<InventoryItem> inventory = new List<InventoryItem>();
+
     private InventoryUIManager inventoryUIManager;
 
     void Start()
